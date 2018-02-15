67fa1e5491b30b58d859734499beea0f735233c7
Baptiste DECHERF <baptiste.decherf@estiam.com>
2018-02-15 12:31:38 +0100

    add wordpress

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
-	echo "Hello world !";
-	var_dump($_SERVER);
-?>
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-<!--
-	git add . 
-	git commit -m "Commit"
-	git push https://github.com/thompson45/test.git
--->
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
