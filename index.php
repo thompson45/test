ec954664781b15ffe10844a385f37a0d3688bb8b
Baptiste DECHERF <baptiste.decherf@estiam.com>
2018-02-15 12:13:24 +0100

    Commit

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php echo "Hello world !"; ?>
+<?php
+	echo "Hello world !";
+	var_dump($_SERVER);
+?>
+
+<!--
+	git add . 
+	git commit -m "Commit"
+	git push https://github.com/thompson45/test.git
+-->
